298c5152291923c4c01c592129adc4ab20af9583
Rama Shema [MacBookPro] <ramashema@gmail.com>
2021-03-28 21:42:23 +0300

    Save task to the database and display the task to the home page

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const passport = require("passport");
 const User = require("../models/user");
+const Task = require("../models/task");
 
-/*define router*/
+/*TODO: define router*/
 const router = express.Router();
 
-/*make important variables available across multiple views*/
+/*TODO: make important variables available across multiple views*/
 router.use(function (req, res, next){
     res.locals.currentUser = req.user;
     res.locals.infos = req.flash("info");
@@ -13,7 +14,7 @@ router.use(function (req, res, next){
     next();
 });
 
-/*the function to verify if user have logged to access to the protected pages*/
+/*TODO: verify if user have logged to access to the protected pages*/
 function ensureAuthenticated(req, res, next){
     if(req.isAuthenticated()){
         next();
@@ -23,21 +24,19 @@ function ensureAuthenticated(req, res, next){
     }
 }
 
-/*routes to the public pages*/
+/*TODO: routes to the index page*/
 router.get("/", function (req, res){
    /*the index page show the welcome messages and request user to login or register*/
     res.render("index");
 });
 
-/*load the form for user registration*/
-
-router.get("/users/registration", function (req, res, next){
+/*TODO: load the form for user registration*/
+router.get("/users/registration", function (req, res){
     /*load the view with the user creation form*/
     res.render("users/registration");
 });
 
-/*register user from the data received from the form*/
-
+/*TODO: register user from the data received from the form*/
 router.post("/users/registration", function (req, res, next){
     /*receive data from the form*/
         const firstname = req.body.firstname;
@@ -71,8 +70,8 @@ router.post("/users/registration", function (req, res, next){
         });
 });
 
-/*path that will launch the login page*/
-router.get("/users/login", function (req, res, next){
+/*TODO: path that will launch the login page*/
+router.get("/users/login", function (req, res){
     res.render("users/login");
 });
 
@@ -83,21 +82,65 @@ router.post("/users/login", passport.authenticate("localStrategy", {
     failureFlash: true
 }));
 
-/*logout process*/
+/*TODO: logout process*/
 router.get("/users/logout", ensureAuthenticated, function (req, res){
     req.logout();
     req.flash("info", "You have successfully logged out");
     res.redirect("/");
 });
 
-
-
-/*the path to home page*/
-router.get("/home",  ensureAuthenticated, function (req, res){
-    res.render("home");
+/*TODO: the path to home page*/
+router.get("/home",  ensureAuthenticated, function (req, res, next){
+    /*TODO: fetch the list of tasks belong to login user from the database */
+    const userID = req.user._id;
+    Task.find({ owner: userID }).sort({ priority: "descending" }).exec(function (error, tasks){
+        if(error){ return next(error); }
+        /*TODO: render the view with tasks*/
+        res.render("home", { tasks: tasks });
+    });
 });
 
+/*TODO: the path to launch the form for adding new task*/
+router.get("/tasks/add", ensureAuthenticated, function (req, res){
+    res.render("tasks/add");
+});
 
+/*TODO: the path to process the storing of task*/
+router.post("/tasks/add", ensureAuthenticated, function (req, res){
+   /*TODO: receive data from the form and validate the presence of required fields*/
+    const owner = req.user._id;
+    const title = req.body.title;
+    const description = req.body.description;
+    const priority = Number(req.body.priority);
+
+    /*validate*/
+    if(!title || !description){
+        req.flash("error", "Title or description field can not be empty");
+        res.redirect("/tasks/add");
+    }
+
+    if(title.length < 10 || description.length < 10){
+        req.flash("error", "Invalid title of description length");
+        res.redirect("/tasks/add");
+    }
+
+    if(priority < 0 || priority > 10){
+        req.flash("error", "Task priority should be between 1 and 10");
+        res.redirect("/tasks/add");
+    }
+
+    const newTask = new Task({
+        owner: owner,
+        title: title,
+        description: description.trim(),
+        priority: priority
+    });
+
+    newTask.save().then(function (){
+        req.flash("info", "New task added successfully");
+        res.redirect("/home");
+    });
+});
 
 /*export the module to be used in other files*/
 module.exports = router;
